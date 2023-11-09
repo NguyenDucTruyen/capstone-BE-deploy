7d9aba671e68242bdb304d56a2e29f44a488158d
linhprovip2002 <dangquangnhatlinh123@gmail.com>
2023-11-09 19:35:50 +0700

    fix/allow access origin

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,10 @@ import express from 'express';
 import routers  from './router/index';
 import { dbConfig } from './database/config';
 import { errorHandler } from './middleware';
+import cors from 'cors';
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 app.use('/api', routers);
