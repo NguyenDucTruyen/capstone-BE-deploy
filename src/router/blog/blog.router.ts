9fd8b78a91b45204f469421ffed77e6dc10eded2
linhprovip2002 <dangquangnhatlinh123@gmail.com>
2023-11-12 19:03:58 +0700

    chore/public blog

--- src/router/blog/blog.router.ts
+++ src/router/blog/blog.router.ts
@@ -3,7 +3,7 @@ import { blogController } from './index'
 import { checkAuthor, verify } from '../../middleware';
 const router = express.Router();
 
-router.get('/', verify, blogController.getBlogs);
+router.get('/', blogController.getBlogs);
 router.post('/', verify, blogController.createBlog);
 router.patch('/:id', verify, blogController.updateBlog);
 
